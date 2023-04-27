8506d33ca23d228a489e95b156cee3228d0716b7
Steven Perez <prez.stvn@gmail.com>
2023-04-27 11:30:19 -0500

    lot of commits here sorry

--- server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
+++ server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -1,7 +1,7 @@
 package com.techelevator.tenmo.controller;
 
 import com.techelevator.tenmo.dao.account.AccountDao;
-import com.techelevator.tenmo.dao.account.JdbcAccountDao;
+
 import com.techelevator.tenmo.model.Account;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -16,14 +16,17 @@ import java.math.BigDecimal;
 @PreAuthorize("isAuthenticated()")
 public class AccountController {
     private AccountDao dao;
-    public AccountController(AccountDao accountDao) { this.dao = accountDao;}
+
+    public AccountController(AccountDao accountDao) {
+        this.dao = accountDao;
+    }
 
 
     //TODO: add transaction status to transaction class and table, possibly as an enumerated type.
-    @RequestMapping(path = "/{id}", method =  RequestMethod.GET)
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
     public Account get(@PathVariable int id) {
         Account account = dao.get(id);
-        if(account == null) {
+        if (account == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Account Not Found");
         } else return account;
     }
@@ -38,7 +41,7 @@ public class AccountController {
     @PreAuthorize("hasAnyRole('ADMIN', 'CREATOR')")
     public Account update(@Valid @RequestBody Account account, @PathVariable int id) {
         Account updatedAccount = dao.update(account, id);
-        if(updatedAccount == null) {
+        if (updatedAccount == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Account Not Found");
         } else return updatedAccount;
     }
